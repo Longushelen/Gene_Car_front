371ca49922c67560ca056b5ccd3210f3b0f508dc
Longushelen <86008239+Longushelen@users.noreply.github.com>
2022-11-10 22:37:56 +0900

    백단 연결 config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {  
+    proxy: "http://localhost:8888"  
+  },  
+  chainWebpack: config => {  
+    const svgRule = config.module.rule("svg");    
+    svgRule.uses.clear();    
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");  
+  }
 })
